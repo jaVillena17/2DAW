424b0b65a04fb68bf3e584cce392b07cc049c3bd
unknown <javillena13@gmail.com>
2024-10-18 17:39:32 +0200

    order

--- HLC/EjerciciosIF/ejercicio6.py
+++ HLC/EjerciciosIF/ejercicio6.py
@@ -1,7 +1,7 @@
 try:    
     amount = int(input("Introduce el importe de tu compra para saber si tienes descuento"))
     if(amount > 300):
-        off = amount - (amount*20/10)
+        off = amount - (amount*20/100)
         print("Tienes un 20% de descuento en tu compra. El precio se queda en",off,"€")
     else:
         print("No tienes descuento en tu compra de",amount,"€")

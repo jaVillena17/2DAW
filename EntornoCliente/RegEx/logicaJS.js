a5fd8c12cf01228206ea7d8b67a2afa36983028e
UsuarioT <javillena13@gmail.com>
2024-11-07 17:37:38 +0100

    RegEx

---
+++ EntornoCliente/RegEx/logicaJS.js
@@ -0,0 +1,45 @@
+function verEnunciado(){
+    //Creamos una variable donde señalamos el parrafo y el boton que modificaremos
+    let p = document.getElementById("enunciadoID");
+    let boton = document.getElementById("botonEnun");
+    //Si es parrafo está vacio, imprimos el enunciado y cambiamos que el boton muestre Ocultar Resultado
+    if(p.innerHTML.length==0){
+        p.innerHTML = "Realiza la lógica de programación para que el usuario introduzca tres valores por teclado. Si al menos uno de ellos es mayor de 10, mostrar en una ventana emergente \"Alguno mayor que 10\". En caso contrario mostrar \"Ninguno es mayor que 10'\"";
+        console.log("Mensaje de prueba en consola")
+        boton.value = "Ocultar Enunciado"
+    }
+    //En el caso de que el parrafo tenga algo, lo reiniciamos
+    else{
+        p.innerHTML="";
+        boton.value = "Ver Enunciado"
+    }
+}
+
+function verResultado(){
+    //Creamos una variable donde señalamos el parrafo y el boton que modificaremos
+    let p = document.getElementById("resolucionID");
+    let boton = document.getElementById("botonRes");
+    //Si el parrafo está vacio, realizamos el ejercicio
+    if(p.innerHTML.length==0){
+        //Iniciamos la cadena
+        let str = "pe.p-Guardiola@gmail.site";
+        //Iniciamos la reges
+        const regex = /^\w*\.?-?\w*\.?-?\w*@gmail(.com)?(.com.es)?(.site)?$/;
+
+        p.innerHTML =""+ regex.test(str);
+        
+        //do{
+            //str = prompt("Introduce du e-Mail");
+
+        //}while(!regex.test(str))
+
+        //Cambioamos el texto que muestra el boton
+        boton.value = "Ocultar Resultado";
+    }
+    //Si el ejercicio ya está resuelto y en pantalla, lo reiniciamos
+    else{
+        p.innerHTML="";
+        boton.value = "Ver Enunciado";
+    }
+    
+}

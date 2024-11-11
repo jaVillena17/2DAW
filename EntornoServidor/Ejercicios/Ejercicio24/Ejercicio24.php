b7aab34218ed663f78dbbe31d152d0428be93e35
unknown <javillena13@gmail.com>
2024-11-11 15:41:07 +0100

    ah

---
+++ EntornoServidor/Ejercicios/Ejercicio24/Ejercicio24.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        div{
+            border: 1px solid black;
+            width: 400px;
+            height: 240px;
+            background-color: aquamarine;
+            margin: 0 auto;
+            border-radius: 10px;
+            padding: 10px;
+        }
+        table{
+            border-collapse: collapse;   
+            margin: 0 auto;
+            background-color: rgb(255, 194, 255);
+        }
+        td{
+            border: 1px solid black;
+            padding: 5px;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        //Fecha del día metido por el usuario
+        $fecha = strtotime($_POST["fecha"]);
+
+        
+        if ($fecha > strtotime("-1 day") && $fecha < strtotime("+1 month") && is_numeric($_POST["SegSo"])){
+            echo "<div><h3>Su cita:</h3><hr>";
+            echo "<table>";
+            echo "<tr><td><b>Paciente:</b></td><td>$_POST[nombre]<br></td></tr> ";
+            echo "<tr><td><b>Numero Seg.Social:</b></td><td>$_POST[SegSo]<br></td></tr> ";
+            echo "<tr><td><b>Tipo de Consulta:</b></td><td>$_POST[consulname]<br></td></tr> ";
+            echo "<tr><td><b>Fecha:</b></td><td>$_POST[fecha]<br></td></tr> ";
+            echo "<tr><td><b>Horario:</b></td><td>$_POST[hor]<br></td></tr> ";
+            echo "</table>";
+            echo "</div>";
+        }else{
+            echo "<div><h3>Operación no completada</h3><hr>";
+            echo "<p>Introduzca una fecha válida</p>";
+        }
+        
+
+
+
+    ?>
+</body>
+</html>

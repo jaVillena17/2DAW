ed3b05748d5da2f7516491d98454ea0f9f1adba8
UsuarioT <javillena13@gmail.com>
2024-11-19 21:38:08 +0100

    Todo guay

--- EntornoCliente/ra3/ejercicio5/logicaJS.js
+++ EntornoCliente/ra3/ejercicio5/logicaJS.js
@@ -4,7 +4,7 @@ function verEnunciado(){
     let boton = document.getElementById("botonEnun");
     //Si es parrafo está vacio, imprimos el enunciado y cambiamos que el boton muestre Ocultar Resultado
     if(p.innerHTML.length==0){
-        p.innerHTML = "Realiza una aplicación web dónde se solicite por pantalla, mediante una ventana emergente, al usuario una cadena y el carácter buscado.Crea una función recursiva que cuente el número de veces que el carácter introducido por el usuario aparece en la cadena introducida por el usuario."
+        p.innerHTML = "Realiza una aplicación web dónde se solicite por pantalla, mediante una ventana emergente, al usuario un código de cliente. <br>Dicho código contiene tres partes separadas por un guión:<br>- Dos caracteres:  CP: cliente particular, CE: empresa <br>- Dos dígitos: 10 Local, 11 Autonómico, 12 Nacional, 20 Internacional<br>- Una cifra que indica el número de años de antigüedad del cliente.<br> Crea una función que decodifique dicho código de cliente. Adicionalmente, si el código es erróneo la función deberá informar del tipo de error o errores."
         boton.value = "Ocultar Enunciado"
     }
     //En el caso de que el parrafo tenga algo, lo reiniciamos
@@ -20,13 +20,10 @@ function verResultado(){
     let boton = document.getElementById("botonRes");
     //Si el parrafo está vacio, realizamos el ejercicio
     if(p.innerHTML.length==0){
-        //Solicitamos que introduzca una frase
-        let cad = prompt("Introduce una frase");
-        //Y después una letra
-        let letra = pedirLetra();
-        //Llamamo a la función
-        let cont = recursiva(cad, letra);
-        p.innerHTML = `El carácter ${letra} aparece ${cont} veces en la cadena`;
+        //Solicitamos que introduzca un código de cliente
+        let cod = prompt("Introduce código de cliente. Recuerda que debe introducir los datos separados por guiones");
+
+        p.innerHTML = decode(cod);
 
         //Cambiamos el texto que muestra el boton
         boton.value = "Ocultar Resultado";
@@ -38,31 +35,35 @@ function verResultado(){
     }
 }
 
-//Función recursiva para contar apariciones
-function recursiva(cadena, letra){
-    //Sacamos el ultimo indice en el que encontramos la letra
-    let index = cadena.lastIndexOf(letra);
-    //Cortamos la cadena desde el principio hasta que se encuentra esa ultima letra
-    let cadenaCortada = cadena.substring(0, index);
-    //Si el indice es -1 (lo que devuelve lastIndexOf al no encontrar nada), salimos de la funcuón recursiva porque ya no quedan letras que contar
-    if(index == -1){
-        return 0;
+function decode(cadena){
+    //Creamos la expresión regular que comprueba que es válido
+    const regEx = /^C[PE]-10-\d|(C[PE]-11-\d)|(C[PE]-12-\d)|(C[PE]-20-\d)$/;
+    //Si no es válido, comprobamos por qué
+    if(!regEx.test(cadena)){
+        if(cadena.charAt(1) != "P" && cadena.charAt(1) != "E") return "<b>Error: </b>El tipo de cliente introducido no es correcto"
+        else if(cadena.charAt(2) != "-" || cadena.charAt(5) != "-") return "<b>Error: </b>Introduce el código separado por guiones"
     }else{
-        //Si lo ha encontrado, sumamos 1 y volvemos a llamar a la funcion con la cadena recortada
-        return 1 + recursiva(cadenaCortada, letra)
+        //Si lo es, sacamos el tipo de cliente
+        let cliente = (cadena.charAt(1) == "P") ? " cliente particular" : "empresa"
+        //El origen
+        let origen = "";
+        let tmp = cadena.substring(3,5);
+        switch(tmp){
+            case "10":
+                origen = "local";
+                break;
+            case "11":
+                origen = "autonómico";
+                break;
+            case "12":
+                origen = "nacional";
+                break;
+            case "20":
+                origen = "internacional"
+                break;
+       }
+       //Devolvemos la cadena resultado
+        return `El código introducido pertenece a un ${cliente} de origen ${origen} con ${cadena.charAt(6)} años de antiguedad`
     }
-}
-//Función recursiva para pedir una letra
-function pedirLetra(){
-    //Declaramos la variable regex que comprueba que se introduce una letra
-    const regEx = /[A-Za-z]/
-    //Pedimos letra
-    let letra = prompt("Introduce una letra")
-    if(regEx.test(letra)){
-        return letra;
-    }
-    else{
-        alert("Debe ser una letra")
-        return pedirLetra();
-    }
+
 }
---
+++ EntornoCliente/ra3/ejercicio7/logicaJS.js
@@ -0,0 +1,37 @@
+function verEnunciado(){
+    //Creamos una variable donde señalamos el parrafo y el boton que modificaremos
+    let p = document.getElementById("enunciadoID");
+    let boton = document.getElementById("botonEnun");
+    //Si es parrafo está vacio, imprimos el enunciado y cambiamos que el boton muestre Ocultar Resultado
+    if(p.innerHTML.length==0){
+        p.innerHTML = "Realiza una aplicación web dónde se solicite por pantalla, mediante una ventana emergente, al usuario una lista de palabras separadas por coma. Crea una función que reciba como parámetro dicha lista. La función deberá agrupar las palabras que comienzan por la misma letra en objetos de la forma inicial: lista de palabras. No se debe distinguir entre mayúsculas y minúsculas. Las palabras deben mantener su tipo de mayúsculas o minúsculas."
+        boton.value = "Ocultar Enunciado"
+    }
+    //En el caso de que el parrafo tenga algo, lo reiniciamos
+    else{
+        p.innerHTML="";
+        boton.value = "Ver Enunciado"
+    }
+}
+
+function verResultado(){
+    //Creamos una variable donde señalamos el parrafo y el boton que modificaremos
+    let p = document.getElementById("resolucionID");
+    let boton = document.getElementById("botonRes");
+    //Si el parrafo está vacio, realizamos el ejercicio
+    if(p.innerHTML.length==0){
+        //Solicitamos que introduzca un código de cliente
+        let cad = prompt("Introduce una serie de palabras separadas por comas");
+
+
+        p.innerHTML = "";
+
+        //Cambiamos el texto que muestra el boton
+        boton.value = "Ocultar Resultado";
+    }
+    //Si el ejercicio ya está resuelto y en pantalla, lo reiniciamos
+    else{
+        p.innerHTML="";
+        boton.value = "Ver Enunciado";
+    }
+}

97f60c34b053657cf6e2c7ea20e3f148a7620c24
unknown <javillena13@gmail.com>
2025-01-07 19:32:47 +0100

    databases

--- EntornoServidor/BasesDeDatos/ejercicio5.sql
+++ EntornoServidor/BasesDeDatos/ejercicio5.sql
@@ -22,3 +22,25 @@ where nombreJuego like "Billar%";
 /*5*/
 select IdJugador, concat(nombreJugador, ap1Jug) as "Nombre y Apellido", telfJug, year(current_date()) - year(fechaN) as "Edad"
 from jugadores;
+/*6*/
+select IdJugador, concat(nombreJugador, ap1Jug) as "Nombre y Apellido", fechaN,
+case 
+when Date_Format(current_date(),'%j') >= Date_Format(fechaN,'%j') then year(current_date()) - year(fechaN)
+else year(current_date()) - year(fechaN) -1
+end as "Edad"
+from jugadores;
+/*7*/
+create view EdadesJugadores as select IdJugador, concat(nombreJugador, ap1Jug) as "Nombre y Apellido", fechaN,
+case 
+when Date_Format(current_date(),'%j') >= Date_Format(fechaN,'%j') then year(current_date()) - year(fechaN)
+else year(current_date()) - year(fechaN) -1
+end as "Edad"
+from jugadores;
+/*8*/
+select jugadores.nombreJugador, jugadores.ap1Jug, jugadores.telfJug, EdadesJugadores.Edad
+from jugadores
+inner join EdadesJugadores on jugadores.IdJugador = EdadesJugadores.IdJugador
+where edad Between 23 AND 30;
+/*9*/
+update juegos
+set  nombreJuego = replace(nombreJuego, 'Poker', 'Póquer');

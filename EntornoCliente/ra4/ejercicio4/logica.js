3c94acb83f80096ed7f767beb16e29bdd191926b
unknown <javillena13@gmail.com>
2025-01-21 12:42:47 +0100

    tpv update

---
+++ EntornoCliente/ra4/ejercicio4/logica.js
@@ -0,0 +1,13 @@
+//Función con el que vamos a transladar nuestas mesas del html a un array de objeto Mesa
+function crearMesas(){
+    
+} 
+
+
+class Mesa{
+    //Constructor de la clase mesa
+    constructor(ubicacion, clientes){
+        this._ubicacion = ubicacion;
+        this._clientes = clientes;
+    }
+}

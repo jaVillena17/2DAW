e755f75bc7c5442c9c0c7b7352999dfb6aa40c32
jaVillena17 <javillena13@gmail.com>
2025-02-06 12:52:32 +0100

    añadir funcion testTurn y gestionar primer movimiento

--- InterfacesWeb/Actividades/Actividad4.3/snake.js
+++ InterfacesWeb/Actividades/Actividad4.3/snake.js
@@ -2,14 +2,26 @@ let canvas;
 let ctx;
 let runnnin = true;
 //Posicion inicial
-let x = 250;
-let y = 250;
 
 let lastAction = null;
 const KEY_LEFT = "ArrowLeft";
 const KEY_UP = "ArrowUp";
 const KEY_RIGHT = "ArrowRight";
 const KEY_DOWN = "ArrowDown";
+
+//Mapa que contiene los tramos de la serpiente, coordenadas, direccion y giro
+let snakeWidth = 1;
+let snake = new Map();
+snake.set(snakeWidth, [10,10,"null", "null"]);
+snakeWidth++;
+snake.set(snakeWidth, [25,10, "null", "null"]);
+snakeWidth++;
+snake.set(snakeWidth, [40,10, "null", "null"]);
+snakeWidth++;
+
+//Creamos un array con los giros
+let turns = new Map();
+
 
 document.addEventListener("keydown", function(e){
     lastAction = e.code;
@@ -18,36 +30,50 @@ document.addEventListener("keydown", function(e){
 window.addEventListener("load", iniciar, false)
 
 function iniciar(){
-    if(runnnin){
-        canvas = document.getElementById("lienzo")
-        ctx = canvas.getContext('2d');
-        run();
-    }   
+    canvas = document.getElementById("lienzo")
+    ctx = canvas.getContext('2d');
+    run();
 }
 
 function run(){
-    requestAnimationFrame(run);
-    move();
-    printCanvas();
+    if(runnnin){
+        requestAnimationFrame(run);
+        move();
+        printCanvas();
+    }
+    
 }
 
 function move(){
+    //Sacamos las coordenadas de la cabeza
+    let head = snake.get(1);
+    let x = head[0];
+    let y = head[1];
+
     if(lastAction === KEY_LEFT){
-        x -= 5;
+        x -= 2;
+        snake.set(1, [x,y, "left", "none"]);
     }else if(lastAction === KEY_RIGHT){
-        x += 5;
-    }
-    else if(lastAction === KEY_UP){
-        y -= 5;
-    }
-    else if(lastAction === KEY_DOWN){
-        y += 5;
+        x += 2;
+        snake.set(1, [x,y, "right", "none"]);
+    }else if(lastAction === KEY_UP){
+        y -= 2;
+        snake.set(1, [x,y, "up", "none"]);
+    }else if(lastAction === KEY_DOWN){
+        y += 2;
+        snake.set(1, [x,y, "down", "none"]);
     }
 
-    if (x >= canvas.width) x = 0;
-    if (x < 0) x = canvas.width;
-    if (y >= canvas.height) y = 0;
-    if (y < 0) y = canvas.height;
+    if(x < 0 || x > canvas.width || y < 0 || y > canvas.width){
+        runnnin = false;
+        alert("Has perdido")
+    }else{
+
+        
+    }
+}
+function testTurn(index){
+
 }
 
 function printCanvas(){
@@ -55,5 +81,8 @@ function printCanvas(){
     image.src = "./Assets/background.png";
     ctx.drawImage(image, 0 ,0)
     ctx.fillStyle='rgb(104, 29, 29)';
-    ctx.fillRect(x,y,15,15);
+    snake.forEach(tramo => {
+        ctx.fillRect(tramo[0],tramo[1],15,15);
+    })
+    //ctx.fillRect(x,y,15,15);
 }
